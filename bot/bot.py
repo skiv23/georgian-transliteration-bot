733a463be5796ccd33a88670564e57046d7811f0
Sergey Bondar <sbondar@milosolutions.com>
2022-03-30 19:21:17 +0400

    initial

---
+++ bot/bot.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import os
+
+from telebot import async_telebot, types
+
+from transliterate import Transliterate
+
+bot = async_telebot.AsyncTeleBot(os.environ["TOKEN"], parse_mode=None)
+
+chats = {}
+
+
+@bot.message_handler(commands=['start'])
+async def send_welcome(message):
+    await bot.reply_to(
+        message,
+        "Hello. This bot allows you to transliterate russian text into Georgian letters."
+        "Type /transliterate to start or /help to see the list of available characters"
+    )
+
+@bot.message_handler(commands=['help'])
+async def send_welcome(message):
+    await bot.reply_to(
+        message,
+        "Here's the list of available characters: а=ა б=ბ г=გ д=დ е=ე  в=ვ з=ზ т(ь)=თ и=ი у=უ к=კ л=ლ м=მ н=ნ о=ო "
+        "п=პ ж=ჟ р= რ с=ს т=ტ пь=ფ кь=ქ гъ=ღ кх=ყ ш=შ ч=ჩ ц=ც дз=ძ цъ=წ чъ=ჭ х=ხ дж=ჯ h(хь)=ჰ"
+    )
+
+
+@bot.message_handler(commands=['transliterate'])
+async def transliterate(message):
+    chat_id = message.chat.id
+    chats[chat_id] = Transliterate(message.chat.id, bot)
+    await bot.reply_to(message, "Paste in the text you want to transliterate")
+
+
+@bot.message_handler(func=lambda message: True)
+async def accept_message(message):
+    chat_id = message.chat.id
+    if chat_id in chats:
+        result = await chats[chat_id].add_message(message)
+        if result:
+            del chats[chat_id]
+
+
+asyncio.run(bot.polling())
---
+++ bot/transliterate.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass, field
+from typing import List
+
+from telebot.async_telebot import AsyncTeleBot
+
+
+@dataclass
+class Transliterate:
+    chat_id: int
+    bot: AsyncTeleBot
+    messages: List[str] = field(default_factory=list)
+    initial_message: str = ""
+    characters: str = ""
+    mapping = {
+        "ა": "а", "ბ": "б", "გ": "г", "დ": "д", "ე": "е", "ვ": "в", "ზ": "з", "თ": "т",
+        "ი": "и", "უ": "у", "კ": "к", "ლ": "л", "მ": "м", "ნ": "н", "ო": "о", "პ": "п", "ჟ": "ж", "რ": "р",
+        "ს": "с", "ტ": "т", "ფ": "п", "ქ": "к", "ღ": "р", "ყ": "кх", "შ": "ш", "ჩ": "ч", "ც": "ц", "ძ": "дз",
+        "წ": "ц", "ჭ": "ч", "ხ": "х", "ჯ": "дж", "ჰ": "х"
+    }
+
+    async def add_message(self, message):
+        text = message.text
+
+        if not self.initial_message:
+            self.initial_message = text.lower()  # Georgian language doesn't have letter case
+            await self.bot.send_message(
+                message.chat.id,
+                f"Type in the characters you want to use",
+            )
+
+        elif not self.characters:
+            self.characters = text
+
+        if self.initial_message and self.characters:
+            return await self.transliterate()
+        return False
+
+    async def transliterate(self):
+        try:
+            replaced_chars = []
+            for char in list(self.characters):
+                if self.mapping[char] in replaced_chars:
+                    await self.bot.send_message(
+                        self.chat_id,
+                        f"{self.mapping[char]} was already replaced, transliteration was not applied for {char}",
+                    )
+                else:
+                    self.initial_message = self.initial_message.replace(self.mapping[char], char)
+                    replaced_chars.append(self.mapping[char])
+            await self.bot.send_message(
+                self.chat_id,
+                self.initial_message,
+            )
+        except KeyError:
+            await self.bot.send_message(
+                self.chat_id,
+                "Something went wrong, check your message or characters",
+            )
+        return True
